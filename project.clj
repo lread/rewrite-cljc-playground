3662c8e49ef17a2cf996252813f4500d2f3dd949
lread <lee@dlread.com>
2019-05-16 12:24:04 -0400

    Inch towards deps.edn
    Still using leiningen, but project deps are now in deps.edn.

--- project.clj
+++ project.clj
@@ -7,9 +7,10 @@
             :year 2015
             :key "mit"}
 
-  :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.10.520"]
-                 [org.clojure/tools.reader "1.3.2"]]
+  :plugins [[lein-tools-deps "0.4.5"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
+
   :source-paths ["src"]
 
   :clean-targets ^{:protect false} [:target-path :compile-path "out"]
@@ -19,17 +20,14 @@
                          "reporters" ["progress" "junit"]
                          "junitReporter" {"outputDir" "target/out/test-results/cljs"}}}}
 
-  :profiles {:1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
-             :kaocha {:dependencies [[lambdaisland/kaocha "0.0-418"]
-                                     [lambdaisland/kaocha-junit-xml "0.0-70"]]}
-             :fig-test {:dependencies [[com.bhauman/figwheel-main "0.2.1-SNAPSHOT"]]
+  :profiles {:1.9 {:lein-tools-deps/config {:resolve-aliases [:1.9]}}
+             :kaocha {:lein-tools-deps/config {:resolve-aliases [:kaocha]}}
+             :fig-test {:lein-tools-deps/config {:resolve-aliases [:fig-test]}
                         :resource-paths ["target"]}
              :doo-test {:plugins [[lein-doo "0.1.11"]]}
-             :dev {:dependencies [[org.clojure/test.check "0.9.0"]
-                                  [doo "0.1.11"]]
+             :dev {:lein-tools-deps/config {:resolve-aliases [:dev]}
                    :exclusions [org.clojure/clojure]
                    :plugins [[lein-cljsbuild "1.1.7"]]
-                   :source-paths ["test"]
                    :cljsbuild {:builds
                                [{:id "fail-on-warning"
                                  :source-paths ["test"]
@@ -60,22 +58,22 @@
                                             :pretty-print true}}]}}}
 
   :aliases {"kaocha"
-            ^{:doc "base kaocha - use to run all clj tests once"}
+            ^{:doc "base kaocha - use to run all clj tests once for default clojure env"}
             ["with-profile" "+kaocha" "run" "-m" "kaocha.runner"]
 
-            "kaocha-junit"
+            "_kaocha-junit"
             ^{:doc "internal base to configure for kaocha junit"}
             ["kaocha" "--reporter" "documentation" "--plugin" "kaocha.plugin/junit-xml"]
 
             "clj-junit-1.9"
             ^{:doc "run all clj tests under clojure 1.9 - output to junit. For ci server."}
-            ["with-profile" "dev,1.9" "kaocha-junit" "--junit-xml-file" "target/out/test-results/clj-v1.9/results.xml"]
+            ["with-profile" "dev,1.9" "_kaocha-junit" "--junit-xml-file" "target/out/test-results/clj-v1.9/results.xml"]
 
             "clj-junit-1.10"
             ^{:doc "run all clj tests under clojure 1.10 - output to junit. For ci server."}
-            ["with-profile" "dev" "kaocha-junit" "--junit-xml-file" "target/out/test-results/clj-v1.10/results.xml"]
+            ["with-profile" "dev" "_kaocha-junit" "--junit-xml-file" "target/out/test-results/clj-v1.10/results.xml"]
 
-            "clj-all"
+            "_clj-all"
             ^{:doc "internal base to select all clojure versions"}
             ["with-profile" "dev,1.9:dev"]
 
@@ -85,9 +83,9 @@
 
             "clj-test-envs"
             ^{:doc "run all clj tests under all supported versions of clojure - for dev sanity test."}
-            ["clj-all" "kaocha"]
+            ["_clj-all" "kaocha"]
 
-            "cljs-chrome-headless"
+            "_cljs-chrome-headless"
             ^{:doc "internal base to setup for chrome headless"}
             ["with-profile" "doo-test,dev" "doo" "chrome-headless"]
 
@@ -95,17 +93,17 @@
             ^{:doc "compile cljs source and fail on any warning - ignoring fn-deprecations we are ok with"}
             ["cljsbuild" "once" "fail-on-warning"]
 
-            "cljs-node"
+            "_cljs-node"
             ^{:doc "internal base to setup for nodejs"}
             ["with-profile" "doo-test,dev" "doo" "node"]
 
             "cljs-test-chrome-headless"
             ^{:doc "run all cljs tests under chrome headless"}
-            ["cljs-chrome-headless" "test" "once"]
+            ["_cljs-chrome-headless" "test" "once"]
 
             "cljs-test-node"
             ^{:doc "run all cljs tests under nodejs"}
-            ["cljs-node" "node-test" "once"]
+            ["_cljs-node" "node-test" "once"]
 
             "cljs-test-envs"
             ^{:doc "run all cljs tests for all supported environments"}
@@ -117,7 +115,7 @@
 
             "clj-auto-test"
             ^{:doc "run all clj tests, watch and rerun automatically"}
-            ["with-profile" "kaocha" "--watch"]
+            ["kaocha" "--watch"]
 
             "chrome-auto-test"
             ^{:doc "run all cljs tests under chrome, watch and rerun automatically"}
