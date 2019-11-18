0b120c6f1ae12b5b1f1911783269ab9d20851c0e
lread <lee@dlread.com>
2019-11-17 19:05:24 -0500

    Update ref to clj 1472 patch builder script

--- script/graal-deps.sh
+++ script/graal-deps.sh
@@ -6,10 +6,12 @@ is-installed() {
     (set +e; mvn -q dependency:get -Dartifact=org.clojure:clojure:1.10.1-patch_38bafca9_clj_1472_3 -o)
 }
 
+CLJ_GRAAL_DOCS_SHA=5b25cc3b39039ddc2604907b5c2092716955cf68
+
 echo "--checking if patched clojure installed--"
 if is-installed; then
    echo "- patched clojure found"
 else
     echo "- patched clojure not found - installing"
-    bash <(curl -s https://raw.githubusercontent.com/lread/clj-graal-docs/5009e2302fb98bf71f03298e6aa08c682d996a24/CLJ-1472/build-clojure-with-1472-patch.sh)
+    bash <(curl -s "https://raw.githubusercontent.com/lread/clj-graal-docs/${CLJ_GRAAL_DOCS_SHA}/CLJ-1472/build-clojure-with-1472-patch.sh")
 fi
