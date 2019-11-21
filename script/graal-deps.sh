09c18cc6ae11d58d23bc574a67f3411a93dcb82d
lread <lee@dlread.com>
2019-11-21 17:39:41 -0500

    graal-deps.sh now verifies maven install

--- script/graal-deps.sh
+++ script/graal-deps.sh
@@ -2,14 +2,18 @@
 
 set -eou pipefail
 
-is-installed() {
+# constants
+CLJ_GRAAL_DOCS_SHA=5b25cc3b39039ddc2604907b5c2092716955cf68
+
+is-patch-installed() {
     (set +e; mvn -q dependency:get -Dartifact=org.clojure:clojure:1.10.1-patch_38bafca9_clj_1472_3 -o)
 }
 
-CLJ_GRAAL_DOCS_SHA=5b25cc3b39039ddc2604907b5c2092716955cf68
+echo "--validate maven installed--"
+mvn --version
 
 echo "--checking if patched clojure installed--"
-if is-installed; then
+if is-patch-installed; then
    echo "- patched clojure found"
 else
     echo "- patched clojure not found - installing"
