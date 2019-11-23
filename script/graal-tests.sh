bbc615f2b2732940a1b971ebcc203cc347f118f5
lread <lee@dlread.com>
2019-11-23 17:56:04 -0500

    Refactor graal test runner experiments

--- script/graal-tests.sh
+++ script/graal-tests.sh
@@ -9,34 +9,7 @@ echo "--[check GraalVM clojure deps]--"
 
 echo "--[running Clojure tests natively compiled via GraalVM]--"
 
-find-graal-prog() {
-    local prog_name=$1
-    if (set +x; hash "$prog_name" > /dev/null 2>&1); then
-        command -v "$prog_name"
-    elif (set +xu; test -f "${JAVA_HOME}/bin/${prog_name}"); then
-        echo "${JAVA_HOME}/bin/${prog_name}"
-    elif (set +xu; test -f "${GRAALVM_HOME}/bin/${prog_name}"); then
-        echo "${GRAALVM_HOME}/bin/${prog_name}"
-    else
-        echo "!not-found!"
-    fi
-}
-
-GRAAL_NATIVE_IMAGE=$(find-graal-prog native-image)
-if [ "${GRAAL_NATIVE_IMAGE}" == "!not-found!" ]; then
-    GRAAL_GU=$(find-graal-prog gu)
-    if [ "$GRAAL_GU" == "!not-found!" ]; then
-        echo "* error: did not find GraalVM native-image nor GraalVM gu to install it."
-        echo "         ensure progs are on PATH or set GRAALVM_HOME / JAVA_HOME"
-        exit 1
-    fi
-    ${GRAAL_GU} install native-image
-    GRAAL_NATIVE_IMAGE=$(find-graal-prog native-image)
-    if [ "${GRAAL_NATIVE_IMAGE}" == "!not-found!" ]; then
-        echo "* error: odd, I just installed GraalVM native-image but cannot find it."
-        exit 1
-    fi
-fi
+GRAAL_NATIVE_IMAGE="$(script/graal-find-native-image.sh)"
 echo "GraalVM native-image program: ${GRAAL_NATIVE_IMAGE}"
 
 TARGET_EXE=target/native-test-runner
@@ -52,7 +25,9 @@ echo "--generate test runner--"
 rm -rf "${TARGET_RUNNER_DIR}"
 mkdir -p "${TARGET_RUNNER_DIR}"
 clojure "${ALIAS}" \
-        -m clj-graal.gen-vars-run-directly "${TARGET_RUNNER_DIR}"
+        -m clj-graal.gen-test-runner \
+        --dest-dir "${TARGET_RUNNER_DIR}" \
+        test-by-var
 
 echo "--aot compile tests--"
 java -cp "$(clojure ${ALIAS} -Spath):${TARGET_RUNNER_DIR}" \
@@ -83,7 +58,9 @@ else
     TIME_CMD="command time -v"
 fi
 
-${TIME_CMD} ${GRAAL_NATIVE_IMAGE} "${native_image_args[@]}"
+# shellcheck disable=SC2086
+${TIME_CMD} \
+    ${GRAAL_NATIVE_IMAGE} "${native_image_args[@]}"
 echo "--running tests compiled under graal--"
 
 ls -lh ${TARGET_EXE}
