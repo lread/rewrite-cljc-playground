fbcd5de7e1a40b3d90e28e0e62aa5d3351b96a74
lread <lee@dlread.com>
2020-09-02 16:32:21 -0400

    Update status printer for scripts
    The status printer used by internal scripts already colorizes warnings errors
    and info lines. This makes it easier to eyeball output at the terminal.
    
    We now better support use case of pasting output in plain ASCII format.
    
    These changes also clean up support for multi-line status messages and
    give more visual weight to warning and errors.
    
    Closes #56

--- script/helper/status.clj
+++ script/helper/status.clj
@@ -1,11 +1,14 @@
 (ns helper.status
   (:require [clojure.string :as string]))
 
-(def ansi-codes {:inverse "7"
+(def format-to-column 80)
 
-                 :fg-black "30"
+(def ansi-codes {:fg-black "30"
                  :fg-red "31"
+                 :fg-green "32"
                  :fg-yellow "33"
+                 :fg-blue "34"
+                 :fg-white "37"
 
                  :bg-red "41"
                  :bg-strong-red "101"
@@ -17,30 +20,106 @@
 
                  :reset "0"})
 
-(defn ansi-esc* [ & codes ]
-  (str #_"ESC[" "\u001b[" (string/join ";" codes) "m"))
+(defn- ansi-esc* [ & codes ]
+  (str "\u001b[" (string/join ";" codes) "m"))
 
-(defn ansi-esc [ & lookups ]
+(defn- ansi-esc [ & lookups ]
   (apply ansi-esc* (map #(get ansi-codes %) lookups)))
 
-(def fmts {:info (str "\n"
-                      (ansi-esc :bg-green :fg-black) " "
-                      (ansi-esc :bg-cyan :fg-black) " %s "
-                      (ansi-esc :bg-green :fg-black) " " (ansi-esc :reset))
-           :detail "%s"
-           :warn (str "\n"
-                      (ansi-esc :bg-yellow :fg-black) "-"
-                      (ansi-esc :bg-strong-yellow :fg-black) " warning: %s "
-                      (ansi-esc :bg-yellow :fg-black) "-" (ansi-esc :reset))
-           :error (str "\n"
-                       (ansi-esc :bg-red :fg-black) "*"
-                       (ansi-esc :bg-strong-red :fg-black) " error: %s "
-                       (ansi-esc :bg-red :fg-black) "*" (ansi-esc :reset))})
+(defn- visible-length [ s ]
+  (-> s
+      string/split-lines
+      last
+      (string/replace  #"\u001b\[[0-9;]+m" "")
+      count))
+
+(defn- colorize [codes s]
+  (str codes s (ansi-esc :reset)))
+
+(defn- str-repeat [n s]
+  (apply str (repeat n s)))
+
+(defn- pad-right [s to-len c]
+  (str s (str-repeat (- (inc to-len) (visible-length s))
+                     c)))
+
+(defn- left-pad-lines [ lines padding ]
+  (map #(str padding %) lines))
+
+(defn- right-pad-lines [ lines max-len]
+  (map #(pad-right % max-len " ") lines))
+
+(defn- block-lines [lines color max-len]
+  (let [blocked (-> lines
+                    (left-pad-lines " ")
+                    (right-pad-lines max-len))]
+    (map #(colorize color %) blocked)) )
+
+(defn- block-problem-lines [lines color]
+  (block-lines lines color (dec format-to-column)))
+
+(defn- wrap-problem-block [lines prefix suffix color]
+  (concat [(colorize color (pad-right prefix format-to-column "-"))]
+          (map #(str (colorize color " ") % ) lines)
+          [(colorize color (str " " (str-repeat (- format-to-column (count suffix)) "-")
+                                suffix))]))
+
+(defn- format-problem-line [msg {:keys [prefix suffix color-bookends color-msg]}]
+  (let [lines (-> msg
+                  string/split-lines
+                  (block-problem-lines color-msg)
+                  (wrap-problem-block prefix suffix color-bookends))]
+    (str "\n" (string/join "\n" lines))))
+
+(defn- block-info-lines [lines color]
+  (let [max-len (->> lines
+                     (map count)
+                     (filter #(<= % format-to-column))
+                     (apply max)
+                     inc)]
+    (block-lines lines color max-len)))
+
+(defn- bookend-info-lines [lines prefix suffix color]
+  (let [indent (str-repeat (visible-length prefix) " ")
+        indented (->> (rest lines)
+                      (map #(str indent %))
+                      (into [(str (colorize color prefix) (first lines))]))]
+    (concat (butlast indented)
+            [(str (last indented) (colorize color suffix))])))
+
+(defn- add-info-dash-break
+  "Add an hidden-ish dash break that will help bring focus when pasting to plain ASCII"
+  [lines]
+  (let [last-line (last lines)]
+    (concat (butlast lines)
+            [(str last-line
+                  (colorize (ansi-esc :fg-black :bg-black)
+                            (str-repeat (- (inc format-to-column) (visible-length last-line))
+                                        "-")))])))
+
+(defn- format-info-line [msg {:keys [prefix suffix color-bookends color-msg]}]
+  (let [lines (-> msg
+                  string/split-lines
+                  (block-info-lines color-msg)
+                  (bookend-info-lines prefix suffix color-bookends)
+                  (add-info-dash-break))]
+    (str "\n" (string/join "\n" lines))))
 
 (defn line [type msg]
-  (if-let [fmt (get fmts type)]
-    (println (format fmt msg))
-    (throw (ex-info (format "unrecognized type: %s for status msg: %s" type msg) {}))))
+  (println (case type
+             :detail msg
+             :info (format-info-line msg {:prefix "["
+                                          :suffix "]"
+                                          :color-bookends (ansi-esc :bg-green :fg-green)
+                                          :color-msg (ansi-esc :bg-cyan :fg-black)})
+             :warn (format-problem-line msg {:prefix "-WARNING"
+                                             :color-bookends (ansi-esc :bg-yellow :fg-black)
+                                             :color-msg (ansi-esc :bg-strong-yellow :fg-black)})
+             :error (format-problem-line msg {:prefix "-ERROR:"
+                                              :suffix "-"
+                                              :color-bookends (ansi-esc :bg-red :fg-black)
+                                              :color-msg (ansi-esc :bg-yellow :fg-black)})
+             (throw (ex-info (format "scripting error: unrecognized type: %s for status msg: %s" type msg) {})))))
 
 (defn fatal [msg exit-code]
   (line :error msg)
