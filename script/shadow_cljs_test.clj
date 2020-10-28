3da17bd1d4ccec5594c24b1da9fb9922a26d3ff6
lread <lee@dlread.com>
2020-10-28 13:51:57 -0400

    Delete shadow-cljs config file on exit
    Our shadow cljs test script creates config for shadow cljs.
    This file is now automatically deleted even when tests fail.

--- script/shadow_cljs_test.clj
+++ script/shadow_cljs_test.clj
@@ -1,11 +1,11 @@
 #!/usr/bin/env bb
 
 (ns shadow-cljs-test
-  (:require [babashka.classpath :as cp]))
+  (:require [babashka.classpath :as cp]
+            [clojure.java.io :as io]))
 
 (cp/add-classpath "./script")
 (require '[helper.env :as env]
-         '[helper.fs :as fs]
          '[helper.shell :as shell]
          '[helper.status :as status])
 
@@ -21,10 +21,9 @@
 ;; Just one sanity test for now
 (env/assert-min-versions)
 (status/line :info "testing ClojureScript source with Shadow CLJS, node, optimizations: none")
-(try
-  (spit "shadow-cljs.edn" shadow-cljs-cfg)
+(let [shadow-config-file (io/file "shadow-cljs.edn")]
+  (.deleteOnExit shadow-config-file)
+  (spit shadow-config-file shadow-cljs-cfg)
   (shell/command ["clojure" "-M:cljs:test-common:shadow-cljs-test" "compile" "test"])
   (shell/command ["node" compiled-tests])
-  nil
-  (finally
-    (fs/delete-file-recursively "shadow-cljs.edn" true)))
+  nil)
