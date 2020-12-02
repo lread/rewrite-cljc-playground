db6a182d9f1357c37b1cb405acec5b7091d1d5f0
lread <lee@dlread.com>
2020-12-02 13:08:08 -0500

    Update tests to satisfy shadow-cljs
    1. There was something about custom zipper util tests that was
    causing shadow-cljs tests to barf with:
    
    RangeError RangeError: Maximum call stack size exceeded
    
    Changing tests from (let [...] (deftest ...) (deftest ...))
    to: (deftest (let [...] (testing ...) (testing ...)))
    Avoids the error.
    
    2. I also switched from using tools deps to npx to run shadow-cljs
    as shadow-cljs recommends using npx to avoid confusion and
    headaches. It was not the cause, but avoids the question.

--- script/shadow_cljs_test.clj
+++ script/shadow_cljs_test.clj
@@ -11,22 +11,19 @@
 
 (def compiled-tests "target/shadow-cljs/node-test.js")
 
-(def shadow-cljs-cfg {:deps true
+(def shadow-cljs-cfg {:source-paths ["src" "test"]
                       :builds {:test {:target :node-test
                                       :output-to compiled-tests
-                                      ;; shadow-cljs test runner unfortunately does not have exclude
-                                      ;; nor metadata support for test selection
-                                      :ns-regexp "(?<!^rewrite-cljc\\.parser-ns)-test$"
                                       :compiler-options {:warnings
                                                          ;; clj-kondo handles deprecation warnings for us
                                                          {:fn-deprecated false}}}}})
 
-;; Just one sanity test for now
 (env/assert-min-versions)
 (status/line :info "testing ClojureScript source with Shadow CLJS, node, optimizations: none")
 (let [shadow-config-file (io/file "shadow-cljs.edn")]
   (.deleteOnExit shadow-config-file)
   (spit shadow-config-file shadow-cljs-cfg)
-  (shell/command ["clojure" "-M:cljs:test-common:shadow-cljs-test" "compile" "test"])
+  (shell/command [(if (= :win (env/get-os)) "npx.cmd" "npx")
+                  "shadow-cljs" "compile" "test"])
   (shell/command ["node" compiled-tests])
   nil)
